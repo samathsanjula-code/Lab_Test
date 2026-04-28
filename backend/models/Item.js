2c8cee811a3fbd21753f830e284a65bda523567a
Samath Sanjula <samathsanjula@gmail.com>
2026-04-28 11:02:21 +0530

    Frontend and Backend added

---
+++ backend/models/Item.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    warranty: {
+      type: Number,
+      required: [true, "warranty is required"],
+      min: [0, "warranty cannot be zero"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
